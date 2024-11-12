61be26373b91a843415f241d0ce36ef8ae98c357
meicon456 <jocastrore@unal.edu.co>
2024-11-12 01:20:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,6 @@ class Auto:
     if self.motor.registro != self.registro:
         return "Las piezas no son originales"
 
-      if asiento is not None and asiento.registro != self.registro and asiento.registro != self.motor.registro: 
+    if asiento is not None and asiento.registro != self.registro and asiento.registro != self.motor.registro: 
         return "Las piezas no son originales"
     return "Auto original"
