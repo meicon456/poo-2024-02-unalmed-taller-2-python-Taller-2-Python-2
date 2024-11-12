9c0876333b32ffaf96a365311af445f7c1bc05cb
meicon456 <jocastrore@unal.edu.co>
2024-11-12 08:09:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 class Motor:
-  def __init__(self,numeroCilindros: int, tipo: str, registro: int):
+  def __init__(self,numeroCilindros, tipo, registro):
     self.numCilindros = numeroCilindros
     self.tipo = tipo
     self.registro = registro
-  def cambiarRegistro(self, registro:int):
+  def cambiarRegistro(self, registro):
       self.registro = registro
-  def asignarTipo(self, tipo: str):
+  def asignarTipo(self, tipo):
       if tipo == "gasolina" or tipo == "electrico":
         self.tipo = tipo
         
 class Asiento:
-  def __init__(self, color: str, precio: float, registro: int)->None:
+  def __init__(self, color, precio, registro):
     self.color = color 
     self.precio = precio
     self.registro = registro
-  def cambiarColor(self, color: str):
+  def cambiarColor(self, color):
       if color == "rojo" or color == "amarillo" or color == "verde" or color == "negro" or color == "blanco":
         self.color = color
       
 class Auto:
   cantidadCreados=0
-  def __init__(self, marca:  str, modelo: str, precio: float,asientos: list[Asiento],motor: Motor,registro: int):
+  def __init__(self, marca, modelo, precio,asientos,motor,registro):
     self.modelo = modelo
     self.precio = precio
-    self.asientos = [asiento for asiento in asientos if isinstance(asiento, Asiento)] 
+    self.asientos = asientos if isinstance(asientos, list) else []
     self.marca = marca
     self.motor = motor
     self.registro = registro
     Auto.cantidadCreados += 1
   def cantidadAsientos(self):
-    return len([asiento for asiento in asientos if isinstance(asiento, Asiento)])
-  def verificarIntegridad(self)-> bool:
+    return len(self.asientos)
+  def verificarIntegridad(self):
     if self.motor.registro != self.registro:
         return "Las piezas no son originales"
-    
+
     for asiento in self.asientos:
         if asiento.registro != self.registro:
             return "Las piezas no son originales"
